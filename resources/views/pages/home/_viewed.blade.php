6886b8be5cee740b147ba364b56fa4e260355d7b
Zhanuzak <kudaibergenov.zhanuzak@gmail.com>
2021-10-03 23:32:17 +0300

    bg remove

--- resources/views/pages/home/_viewed.blade.php
+++ resources/views/pages/home/_viewed.blade.php
@@ -1,22 +1,22 @@
 @if(isset($views) and $views->count() > 0)
     <section class="section slideshow section--slideshow viewed page-home__viewed">
-        <div class="section__background section__background--pr">
+        {{-- <div class="section__background section__background--pr">
             <img src="{{ asset('img/sections-bg/viewed-bg.jpg') }}" alt="">
             <img src="{{ asset('img/sections-bg/viewed-bg-2.png') }}" alt="" class="viewed__background">
-        </div>
+        </div> --}}
         <div class="container">
             <div class="section__header">
-                <h2 class="section__title section__title--light">Просмотренные товары</h2>
+                <h2 class="section__title section__title--dark">Просмотренные товары</h2>
                 <div class="section__arrows">
                     <div class="section__arrow section__arrow--prev slideshow__arrow--prev">
                         <svg width="37" height="37" viewBox="0 0 37 37" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="18.5" cy="18.5" r="18.5" fill="white"/>
+                            <circle cx="18.5" cy="18.5" r="18.5" fill="#F5F5F5"/>
                             <path d="M14.2929 18.2929C13.9024 18.6834 13.9024 19.3166 14.2929 19.7071L20.6569 26.0711C21.0474 26.4616 21.6805 26.4616 22.0711 26.0711C22.4616 25.6805 22.4616 25.0474 22.0711 24.6569L16.4142 19L22.0711 13.3431C22.4616 12.9526 22.4616 12.3195 22.0711 11.9289C21.6805 11.5384 21.0474 11.5384 20.6569 11.9289L14.2929 18.2929ZM16 18L15 18L15 20L16 20L16 18Z" fill="black"/>
                         </svg>
                     </div>
                     <div class="section__arrow section__arrow--next slideshow__arrow--next">
                         <svg width="37" height="37" viewBox="0 0 37 37" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="18.5" cy="18.5" r="18.5" fill="white"/>
+                            <circle cx="18.5" cy="18.5" r="18.5" fill="#F5F5F5"/>
                             <path d="M14.2929 18.2929C13.9024 18.6834 13.9024 19.3166 14.2929 19.7071L20.6569 26.0711C21.0474 26.4616 21.6805 26.4616 22.0711 26.0711C22.4616 25.6805 22.4616 25.0474 22.0711 24.6569L16.4142 19L22.0711 13.3431C22.4616 12.9526 22.4616 12.3195 22.0711 11.9289C21.6805 11.5384 21.0474 11.5384 20.6569 11.9289L14.2929 18.2929ZM16 18L15 18L15 20L16 20L16 18Z" fill="black"/>
                         </svg>
                     </div>
