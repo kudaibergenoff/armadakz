971f49686f846bcba3e53d90415b7885296ee81f
Zhanuzak <kudaibergenov.zhanuzak@gmail.com>
2021-10-30 13:37:48 +0300

    test 1 commit

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -22,7 +12,7 @@ Route::post('/pay/result', function(Request $request) {
 });
 
 Route::get('test', function() {
-    return "Test";
+    return "Test1";
 });
 
 
