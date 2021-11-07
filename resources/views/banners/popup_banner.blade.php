966f8998d3625f6d3f99ae539cd2e945a1edc150
Zhanuzak <kudaibergenov.zhanuzak@gmail.com>
2021-11-07 22:03:55 +0300

    popup modal styled

--- resources/views/banners/popup_banner.blade.php
+++ resources/views/banners/popup_banner.blade.php
@@ -1,13 +1,13 @@
 @isset($popupBanner)
 <div class="modal fade" id="myModal">
     <button type="button" class="close" data-dismiss="modal" style="color: #fff; margin-top: 25px; margin-right: 25px;">&times;</button>
-    <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-dialog modal-dialog-centered modal-lg" style="width: 800px; height: 250px;">
       <div class="modal-content">
 
         <!-- Modal body -->
         <div class="modal-body" style="padding: 0px;">
             @foreach ($popupBanner as $banner)
-            <a href="{{$banner->link}}">
+            <a href="{{$banner->link}}" target="_blank">
                 <picture>
                     <source srcset="{{ $banner->getImage('images_1580x550') }}" media="(min-width: 1580px)">
                     <source srcset="{{ $banner->getImage('images_1280x450') }}" media="(min-width: 1280px)">
